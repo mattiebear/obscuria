130194199720c0a11b1cea265dd9b3b6c87e5a57
Matthew Sells <matthew.sells@proton.me>
2024-03-25 21:28:45 -0500

    add user auth

--- lib/obscuria_web/router.ex
+++ lib/obscuria_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ObscuriaWeb.Router do
   use ObscuriaWeb, :router
 
+  import ObscuriaWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule ObscuriaWeb.Router do
     plug :put_root_layout, html: {ObscuriaWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -18,8 +21,6 @@ defmodule ObscuriaWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-
-    live "/scribe", PuzzlesLive.Index, :index
   end
 
   # Other scopes may use custom stacks.
@@ -41,6 +42,46 @@ defmodule ObscuriaWeb.Router do
 
       live_dashboard "/dashboard", metrics: ObscuriaWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", ObscuriaWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{ObscuriaWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", AuthLive.UserRegistrationLive, :new
+      live "/users/log_in", AuthLive.UserLoginLive, :new
+      live "/users/reset_password", AuthLive.UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", AuthLive.UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", ObscuriaWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{ObscuriaWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", AuthLive.UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", AuthLive.UserSettingsLive, :confirm_email
+
+      live "/scribe", PuzzlesLive.Index, :index
+    end
+  end
+
+  scope "/", ObscuriaWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{ObscuriaWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", AuthLive.UserConfirmationLive, :edit
+      live "/users/confirm", AuthLive.UserConfirmationInstructionsLive, :new
     end
   end
 end
